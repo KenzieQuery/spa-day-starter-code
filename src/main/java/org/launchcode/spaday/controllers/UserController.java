d9c53e12a3d50a87f4ed4d292106b95ba726dd9a
KenzieQuery <william_query@comcast.com>
2021-07-15 16:06:37 -0600

    ch 13 studio partial

--- src/main/java/org/launchcode/spaday/controllers/UserController.java
+++ src/main/java/org/launchcode/spaday/controllers/UserController.java
@@ -10,24 +10,39 @@ import org.springframework.web.bind.annotation.*;
 public class UserController {
 
     @GetMapping("/add")
-    public String displayAddUserForm() {
+    public String displayAddUserForm(Model model) {
+        model.addAttribute(new User());
         return "user/add";
     }
 
     @PostMapping
     public String processAddUserForm(Model model, @ModelAttribute User user, String verify) {
-        model.addAttribute("user", user);
-        model.addAttribute("verify", verify);
-        model.addAttribute("username", user.getUsername());
-        model.addAttribute("email", user.getEmail());
-        if (user.getPassword().equals(verify)) {
-           return "user/index";
-        }
-        else {
-            model.addAttribute("error", "Passwords do not match");
-            return "user/add";
-        }
+//        model.addAttribute("user", user);
+//        model.addAttribute("verify", verify);
+//        model.addAttribute("username", user.getUsername());
+//        model.addAttribute("email", user.getEmail());
+//        if (user.getPassword().equals(verify)) {
+//           return "user/index";
+//        }
+//        else {
+//            model.addAttribute("error", "Passwords do not match");
+//            return "user/add";
+//        }
 
+        //errors.hasErrors() returns true if the user is not valid
+        //if errors.hasErrors() returns true, re-render the form
+        //else if passwords do not match, re-render the form
+        //else render index.html
+        if(errors.hasErrors()){
+            return "/user/add";
+        }
+        else if(!user.getPassword().equals(verify)){
+            model.addAttribute("error", "Passwords do not match.")
+            return "/user/add";
+        }
+        else{
+            return "/user/index";
+        }
     }
 
 
